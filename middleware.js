0212f69753136d1239edb11099700599d947e087
aliakseividaseu <alexaat@yahoo.co.uk>
2025-02-20 22:45:54 +0000

    updates

--- middleware.js
+++ middleware.js
@@ -3,6 +3,8 @@ const queries = require('./db/queries')
 const google_api = require('./network/google_user_api');
 
 const auth = (req, res, next) => {
+    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(`url: ${fullUrl}`);
     //authorization
     const accessToken = req.header('authorization');
     google_api.getGoogleUser(accessToken)
@@ -24,12 +26,14 @@ const auth = (req, res, next) => {
         //get user by email
         pool.query(queries.findUserByEmail(email), (error, results) => {
                 if (error) {
+                    console.log('findUserByEmail error: ', error)                    
                     res.status(500).end()
-                } else {
+                } else {                   
                     if (results.rows.length == 0) {
                         //Insert user
                         pool.query(queries.addUser(name, email, picture), (error, results) => {
                             if(error){
+                                console.log('addUser error: ', error)     
                                 res.status(500).end()
                             } else {
                                 const user_id = results.rows[0]["id"]
@@ -44,12 +48,12 @@ const auth = (req, res, next) => {
                         const _avatar = results.rows[0]["avatar"];
 
                         if(_name != name){
-                            //update user name              
+                            //update user name 
                             pool.query(queries.updateName(user_id, name))               
                         }
             
                         if(_avatar != picture){
-                            //update user avatar              
+                            //update user avatar      
                             pool.query(queries.updateAvatar(user_id, picture))
                         }
                         req.headers['authorization_id'] = user_id
@@ -60,6 +64,7 @@ const auth = (req, res, next) => {
         }     
     )
     .catch(e => {
+        console.log('error at middleware')
         console.log(`ERROR. ${e}`)
         res.status(401).end()
     })
